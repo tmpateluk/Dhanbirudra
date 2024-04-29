0d592372ba8697d50aae4a9790f4af54532306cc
Tushar Patel <tmpateluk@gmail.com>
2024-04-29 22:31:11 +0100

    ProjectDhanviRudra

---
+++ src/main/java/DhanviRudra/RudraDhanvi.java
@@ -0,0 +1,11 @@
+package DhanviRudra;
+
+public class RudraDhanvi
+{
+    public static void main(String[] args) {
+        System.out.println("Rudra Patel");
+        System.out.println("Dhanvi Patel");
+        System.out.println("Tushar Patel");
+        System.out.println("Bhoomika Patel");
+    }
+}
